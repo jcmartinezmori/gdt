306ea26403823e5c1fc72b17a45da213a4e440a2
Carlos Martinez <jcmartinezmori@Carloss-MacBook-Air-2.local>
2026-07-13 14:59:28 -0600

    .

--- src/figures.py
+++ src/figures.py
@@ -8,10 +8,17 @@ import pickle
 import shutil
 from playwright.async_api import async_playwright
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from pathlib import Path
 import src.instance
 from src.config import *
+
+
+def hex_to_rgba(hex_color, alpha):
+    hex_color = hex_color.lstrip("#")
+    r = int(hex_color[0:2], 16)
+    g = int(hex_color[2:4], 16)
+    b = int(hex_color[4:6], 16)
+    return f"rgba({r}, {g}, {b}, {alpha})"
 
 
 def __draw_stop(folium_map, s, G, rhos, F):
@@ -254,125 +261,191 @@ def diff_neutral_solution_maps(solver_params):
     folium_map.save('./results/maps/html/diff_neutral_coverage_service_plan_{0}.html'.format(solution_filename))
 
 
-def level_of_service(filename, solver_params):
+def level_of_service(solver_params_list):
 
-    instance_filename = filename
-    solution_filename = filename + '_' + solver_params
+    rhos = src.instance.__load_rhos(PLACE)
+    st_pairs = src.instance.__load_st_pairs(PLACE)
+    C = src.instance.__load_C(PLACE)
+    L, L_st = src.instance.__load_L_L_st(PLACE)
 
-    st_pairs = src.instance.__load_st_pairs(instance_filename)
-    L, L_st, C = src.instance.__load_L_L_st_C(instance_filename)
-    T, T_st = src.instance.__load_T_T_st(instance_filename)
+    symbol_map = {
+        1: 'triangle-up',
+        0.8: 'circle'
+    }
 
-    with open('./results/solutions/P_u_{0}.pkl'.format(solution_filename), 'rb') as file:
-        P_u = pickle.load(file)
-    with open('./results/solutions/P_y_{0}.pkl'.format(solution_filename), 'rb') as file:
-        P_y = pickle.load(file)
+    data = []
+    for solver_params in solver_params_list:
 
-    C_dict = {ell: h for ell, h in C}
-    P_u_dict = {ell: h for ell, h in P_u}
-    P_y_dict = {ell: h for ell, h in P_y}
+        budget_factor, ridership_factor = solver_params
 
+        solver_params = 'BUDGET_FACTOR-{0}_RIDERSHIP_FACTOR-{1}'.format(budget_factor, ridership_factor)
+        solution_filename = PLACE + '_' + solver_params
+
+        with open('./results/solutions/P_u_{0}.pkl'.format(solution_filename), 'rb') as file:
+            P_u = pickle.load(file)
+
+        P_u_dict = {ell: h for ell, h in P_u}
+        freq_P_u = {(s, t): 0 for s, t in st_pairs}
+        for s, t in st_pairs:
+            for ell in L_st[(s, t)]:
+                try:
+                    freq_P_u[(s, t)] += 1 / P_u_dict[ell]
+                except KeyError:
+                    pass
+
+        ridership = sum(np.sqrt(rhos[s] * rhos[t]) * np.sqrt(freq) for (s, t), freq in freq_P_u.items())
+        coverage = sum(1 for freq in freq_P_u.values() if freq >= 1/COVERAGE_H)
+        legend = 'Goal: Ridership, Budget Factor: {0}, Ridership Factor: {1}'.format(budget_factor, ridership_factor)
+        data.append((ridership, coverage, ridership_factor, symbol_map[budget_factor], HEXVERMILLION, legend))
+
+        with open('./results/solutions/P_y_{0}.pkl'.format(solution_filename), 'rb') as file:
+            P_y = pickle.load(file)
+        P_y_dict = {ell: h for ell, h in P_y}
+
+        freq_P_y = {(s, t): 0 for s, t in st_pairs}
+        for s, t in st_pairs:
+            for ell in L_st[(s, t)]:
+                try:
+                    freq_P_y[(s, t)] += 1 / P_y_dict[ell]
+                except KeyError:
+                    pass
+
+        ridership = sum(np.sqrt(rhos[s] * rhos[t]) * np.sqrt(freq) for (s, t), freq in freq_P_y.items())
+        coverage = sum(1 for freq in freq_P_y.values() if freq >= 1/COVERAGE_H)
+        data.append((ridership, coverage, ridership_factor, symbol_map[budget_factor], HEXBLUE))
+
+    C_dict = {ell: C[ell]['h'] for ell in C.keys()}
     freq_C = {(s, t): 0 for s, t in st_pairs}
-    freq_P_u = {(s, t): 0 for s, t in st_pairs}
-    freq_P_y = {(s, t): 0 for s, t in st_pairs}
     for s, t in st_pairs:
         for ell in L_st[(s, t)]:
-            if ell in C_dict.keys():
-                freq_C[(s, t)] += 1/C_dict[ell]
-            if ell in P_u_dict.keys():
-                freq_P_u[(s, t)] += 1/P_u_dict[ell]
-            if ell in P_y_dict.keys():
-                freq_P_y[(s, t)] += 1/P_y_dict[ell]
+            try:
+                freq_C[(s, t)] += 1 / C_dict[ell]
+            except KeyError:
+                pass
+    ridership = sum(np.sqrt(rhos[s] * rhos[t]) * np.sqrt(freq) for (s, t), freq in freq_C.items())
+    coverage = sum(1 for freq in freq_C.values() if freq >= 1/COVERAGE_H)
 
-    freq_df = pd.DataFrame([freq_C, freq_P_u, freq_P_y]).T
-    freq_df.columns = ['Current Service Plan', 'Ridership Service Plan', 'Coverage Service Plan']
-    fig = make_subplots(
-        rows=2, cols=1,
-        subplot_titles=(
-            'Distribution of Level of Service with No Transfers ({0:.1f}-Incentive Compatibility)'.format(IC_FACTOR),
-            'Distribution of Level of Service with One Transfer ({0:.1f}-Incentive Compatibility)'.format(IC_FACTOR)
-        ),
-        vertical_spacing=0.175
+    df = pd.DataFrame(data, columns=['ridership', 'coverage', 'ridership_factor', 'symbol', 'hexcolor'])
+    df['color'] = df.apply(lambda row: hex_to_rgba(row['hexcolor'], row['ridership_factor']), axis=1)
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=[coverage],
+            y=[ridership],
+            mode='markers',
+            marker=dict(
+                color=[HEXBLACK],
+                symbol='star',
+                size=20,
+                line=dict(color="black", width=2),
+            ),
+        showlegend=False,
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df['coverage'],
+            y=df['ridership'],
+            mode="markers",
+            marker=dict(
+                symbol=df['symbol'],
+                color=df['color'],
+                size=25,
+            ),
+        showlegend=False
+        )
     )
 
-    colors = [HEXORANGE, HEXBLUE, HEXVERMILLION]
-    for i, col in enumerate(freq_df.columns):
-        x = list(range(len(freq_df[col])))
-        y = np.sort(freq_df[col].values)
-        fig.add_trace(go.Scatter(
-            x=x,
-            y=y,
-            mode='lines',
-            name=f"{col}",
-            line=dict(color=colors[i], width=2),
-            showlegend=True
-        ), row=1, col=1)
-    fig.update_yaxes(
-        row=1, col=1,
-        title_text=r'$\large \textrm{Service Frequency } [\mathtt{min}^{-1}]$',
-        title_font={'size': 20},
-        range=[0 - 0.025, 0.6]
-    )
-    fig.update_xaxes(
-        row=1, col=1,
-        title_text=r'$\large \textrm{Origin-Destination Pairs (sorted by Service Frequency)}$',
-        title_font={'size': 20},
-        range=[125000, 200000]
-    )
-
-    for s, t in st_pairs:
-        for ell1, ell2 in T_st[(s, t)]:
-            if ell1 in C_dict.keys() and ell2 in C_dict.keys():
-                if C_dict[ell1] <= TRANSFER_MIN_H or C_dict[ell2] <= TRANSFER_MIN_H:
-                    freq_C[(s, t)] += 1/max(C_dict[ell1], C_dict[ell2])
-            if ell1 in P_u_dict.keys() and ell2 in P_u_dict.keys():
-                if P_u_dict[ell1] <= TRANSFER_MIN_H or P_u_dict[ell2] <= TRANSFER_MIN_H:
-                    freq_P_u[(s, t)] += 1/max(P_u_dict[ell1], P_u_dict[ell2])
-            if ell1 in P_y_dict.keys() and ell2 in P_y_dict.keys():
-                if P_y_dict[ell1] <= TRANSFER_MIN_H or P_y_dict[ell2] <= TRANSFER_MIN_H:
-                    freq_P_y[(s, t)] += 1/max(P_y_dict[ell1], P_y_dict[ell2])
-
-    freq_df = pd.DataFrame([freq_C, freq_P_u, freq_P_y]).T
-    freq_df.columns = ['Current Service Plan', 'Ridership Service Plan', 'Coverage Service Plan']
-
-    for i, col in enumerate(freq_df.columns):
-        x = list(range(len(freq_df[col])))
-        y = np.sort(freq_df[col].values)
-        fig.add_trace(go.Scatter(
-            x=x,
-            y=y,
-            mode='lines',
-            name=f"{col}",
-            line=dict(color=colors[i], width=2),
-            showlegend=False
-        ), row=2, col=1)
-    fig.update_yaxes(
-        row=2, col=1,
-        title_text=r'$\large \textrm{Service Frequency } [\mathtt{min}^{-1}]$',
-        title_font={'size': 20},
-        range=[0 - 0.025, 0.6]
-    )
-    fig.update_xaxes(
-        row=2, col=1,
-        title_text=r'$\large \textrm{Origin-Destination Pairs (sorted by Service Frequency)}$',
-        title_font={'size': 20},
-        range=[125000, 200000]
-    )
-
-    for annotation in fig['layout']['annotations']:
-        annotation['font'] = {'size': 24}
-        # annotation['y'] = 1.0125
     fig.update_layout(
-        legend={
-            'orientation': 'v', 'entrywidth': 250, 'yanchor': 'top', 'y': 1-0.125/4, 'xanchor': 'left', 'x': 0.125/4,
-            'font': {'size': 20}
-        }
+        xaxis=dict(title='Coverage'),
+        yaxis=dict(title='Ridership'),
     )
-    fig.write_image(
-        './results/figures/los_{0}.pdf'.format(solution_filename),
-        width=1200, height=800
-    )
-    # fig.show()
+
+    fig.show()
+
+
+
+
+
+
+
+# def level_of_service(solver_params):
+#
+#     solution_filename = PLACE + '_' + solver_params
+#
+#     G = src.instance.__load_G(PLACE)
+#     rhos = src.instance.__load_rhos(PLACE)
+#     W, T, F = src.instance.__load_W_T_F(PLACE)
+#     st_pairs = src.instance.__load_st_pairs(PLACE)
+#     C = src.instance.__load_C(PLACE)
+#     L, L_st = src.instance.__load_L_L_st(PLACE)
+#
+#     with open('./results/solutions/P_u_{0}.pkl'.format(solution_filename), 'rb') as file:
+#         P_u = pickle.load(file)
+#     with open('./results/solutions/P_y_{0}.pkl'.format(solution_filename), 'rb') as file:
+#         P_y = pickle.load(file)
+#
+#     C_dict = {ell: C[ell]['h'] for ell in C.keys()}
+#     P_u_dict = {ell: h for ell, h in P_u}
+#     P_y_dict = {ell: h for ell, h in P_y}
+#
+#     freq_C = {(s, t): 0 for s, t in st_pairs}
+#     freq_P_u = {(s, t): 0 for s, t in st_pairs}
+#     freq_P_y = {(s, t): 0 for s, t in st_pairs}
+#     for s, t in st_pairs:
+#         for ell in L_st[(s, t)]:
+#             if ell in C_dict.keys():
+#                 freq_C[(s, t)] += 1/C_dict[ell]
+#             if ell in P_u_dict.keys():
+#                 freq_P_u[(s, t)] += 1/P_u_dict[ell]
+#             if ell in P_y_dict.keys():
+#                 freq_P_y[(s, t)] += 1/P_y_dict[ell]
+#
+#     freq_df = pd.DataFrame([freq_C, freq_P_u, freq_P_y]).T
+#     freq_df.columns = ['Current Service Plan', 'Ridership Service Plan', 'Coverage Service Plan']
+#
+#     fig = go.Figure()
+#
+#     colors = [HEXORANGE, HEXBLUE, HEXVERMILLION]
+#     for i, col in enumerate(freq_df.columns):
+#         x = list(range(len(freq_df[col])))
+#         y = np.sort(freq_df[col].values)
+#         fig.add_trace(go.Scatter(
+#             x=x,
+#             y=y,
+#             mode='lines',
+#             name=f"{col}",
+#             line=dict(color=colors[i], width=2),
+#             showlegend=True
+#         ))
+#     fig.update_yaxes(
+#         title_text=r'$\large \textrm{Service Frequency } [\mathtt{min}^{-1}]$',
+#         title_font={'size': 20}
+#     )
+#     fig.update_xaxes(
+#         title_text=r'$\large \textrm{Origin-Destination Pairs (sorted by Service Frequency)}$',
+#         title_font={'size': 20}
+#     )
+#     fig.show()
+#
+#     for annotation in fig['layout']['annotations']:
+#         annotation['font'] = {'size': 24}
+#         # annotation['y'] = 1.0125
+#     fig.update_layout(
+#         legend={
+#             'orientation': 'v', 'entrywidth': 250, 'yanchor': 'top', 'y': 1-0.125/4, 'xanchor': 'left', 'x': 0.125/4,
+#             'font': {'size': 20}
+#         }
+#     )
+#     fig.write_image(
+#         './results/figures/los_{0}.pdf'.format(solution_filename),
+#         width=1200, height=800
+#     )
+#     # fig.show()
+
+
+
 
 
 async def convert_html_to_images(html_dir, pdf_dir):
@@ -405,13 +478,21 @@ if __name__ == '__main__':
 
     # solver_params = 'BUDGET_FACTOR-{0}_RIDERSHIP_FACTOR-{1}'.format(BUDGET_FACTOR, RIDERSHIP_FACTOR)
     solver_params = 'BUDGET_FACTOR-{0}_RIDERSHIP_FACTOR-{1}'.format(0.8, 0.6)
-    current_map()
-    solution_maps(solver_params)
+    # current_map()
+    # solution_maps(solver_params)
     # diff_gain_solution_maps(solver_params)
     # diff_loss_solution_maps(solver_params)
     # diff_neutral_solution_maps(solver_params)
 
-    # level_of_service(filename, solver_params)
+    solver_params_list = [
+        (0.8, 0.5),
+        (0.8, 0.6),
+        (0.8, 0.7),
+        (1, 0.5),
+        (1, 0.6),
+        (1, 0.7),
+    ]
+    level_of_service(solver_params_list)
 
     html_dir = './results/maps/html'
     pdf_dir = './results/maps/pdf'
